6460e963f2dd4ec16bd2f68e0b300259a268906d
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-11-01 01:01:35 -0400

    cleaned STAI score processing script

--- STAI_scoring.R
+++ STAI_scoring.R
@@ -9,34 +9,29 @@ library(dplyr)
 data_path <- '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/all_participants/STAIscores.csv' #EDIT PATH NAME EACH TIME
 
 #load data
-df_pilot1 <- read_csv(data_path)
+STAI_df <- read_csv(data_path)
 
-#manipulate df to get it to look how you want
-colnames(df_pilot1) <- c('not_included', 'subjectID', paste0('s', 1:20))
-num_rows = nrow(df_pilot1)
+#manipulate df into proper format
+colnames(STAI_df) <- c('not_included', 'subjectID', paste0('s', 1:20))
+num_rows = nrow(STAI_df)
 num_rows
 
-df_pilot1 <- df_pilot1 %>%
+STAI_df <- STAI_df %>%
   slice(2:num_rows) %>%
   select('subjectID', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20')
 
-#getting scores into numeric form so you can add them together later
-print(sapply(df_pilot1, class)) #debugging
-df_pilot1 <- transform(df_pilot1,s1 = as.numeric(s1))
-print(sapply(df_pilot1$s1, class)) #debugging
-df_pilot1 <- df_pilot1 %>%
+#getting STAI scores into numeric form
+STAI_df <- transform(STAI_df,s1 = as.numeric(s1))
+STAI_df <- STAI_df %>%
   mutate(across(paste0('s', 1:20), as.numeric))
 
-
-
-
 #changing non-responses (5) into NaN
-df_pilot1 <- df_pilot1 %>%
+STAI_df <- STAI_df %>%
   mutate(across(paste0('s', 1:20), ~ case_when(. == 5 ~ 0, 
                                                TRUE ~ .)))
 
 #reverse score what needs to be reverse scored
-df_pilot1 <- df_pilot1 %>%
+STAI_df <- STAI_df %>%
   group_by(subjectID) %>%
   mutate(s1 = ifelse(s1== 0, 0, 5 - s1),
          s3 = ifelse(s3== 0, 0, 5 - s3),
@@ -50,26 +45,28 @@ df_pilot1 <- df_pilot1 %>%
 
 
 #get the mean & replace NaNs(0s) w the mean 
-df_pilot1 <- df_pilot1 %>%
+STAI_df <- STAI_df %>%
   mutate(meanVals = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20)/ 20) %>%
   mutate(across(paste0('s', 1:20), ~ case_when(. == 0 ~ meanVals, 
                                                TRUE ~ .)))
 
 #sum STAI score
-df_pilot1 <- df_pilot1 %>%
+STAI_df <-STAI_df %>%
   group_by(subjectID) %>%
   mutate(sumVals = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11 + s12 + s13 + s14 + s15 + s16 + s17 + s18 + s19 + s20)
 
 #classify anxiety levels based on STAI sum
-df_pilot1_STAI <- df_pilot1 %>%
+STAI_df <- STAI_df %>%
   group_by(subjectID) %>%
   mutate(anxiety_level = case_when(
     sumVals <= 37 ~ 'low trait anxiety',
     sumVals >= 38 & sumVals < 44 ~ 'moderate trait anxiety',
     sumVals >= 44 ~ 'high trait anxiety'
   ))
-df_pilot1_STAI
+STAI_df
 
 #save out the file
-write.csv(df_pilot1_STAI, '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/all_participants/STAI_scores_calculated.csv') #EDIT PATH NAME EACH TIME
+write.csv(STAI_df, 'data/STAI.csv')
+write.csv(STAI_df, '/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/all_participants/STAI_scores_calculated.csv') #EDIT PATH NAME EACH TIME
+
 
