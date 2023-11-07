95a0db9c7914bfae35b3463ea74a9f6e499c8ef0
B-Sevchik <78758419+B-Sevchik@users.noreply.github.com>
2023-11-07 14:06:03 -0500

    edited check answer/drag task analysis so it can be run independently
    added proprocessing into the check answer analysis script to create check_answer_df without having to run another script

--- dragtask_checkanswer_analysis.R
+++ dragtask_checkanswer_analysis.R
@@ -1,6 +1,22 @@
 library(plotrix)
 
-check_answer_df <- read_csv(...)
+df <- read_csv('/Users/brookesevchik/Box/Data/Anxiety_Cognitive_Maps/all_participants/combinedData_Anxiety_Cognitive_Maps.csv')
+
+check_answer_df <- df %>%
+  filter(sectionType == 'dragTaskCheckAnswerEvent') %>%
+  select(subject, trialCount, trialAttempt, RT, nCorrect, slot0Acc, slot0CurrentType, slot0CorrectType, slot0CurrentSRC, slot0CorrectSRC, slot1Acc, 
+         slot1CurrentType, slot1CorrectType,slot1CurrentSRC, slot1CorrectSRC,
+         slot2Acc, slot2CurrentType, slot2CorrectType, slot2CurrentSRC, slot2CorrectSRC,
+         slot3Acc, slot3CurrentType, slot3CorrectType, slot3CurrentSRC, slot3CorrectSRC,
+         slot4Acc, slot4CurrentType, slot4CorrectType, slot4CurrentSRC, slot4CorrectSRC,
+         slot5Acc, slot5CurrentType, slot5CorrectType, slot5CurrentSRC, slot5CorrectSRC,
+         slot6Acc, slot6CurrentType, slot6CorrectType, slot6CurrentSRC, slot6CorrectSRC,
+         slot7Acc, slot7CurrentType, slot7CorrectType, slot7CurrentSRC, slot7CorrectSRC,
+         slot8Acc, slot8CurrentType, slot8CorrectType, slot8CurrentSRC, slot8CorrectSRC,
+         slot9Acc, slot9CurrentType, slot9CorrectType, slot9CurrentSRC, slot9CorrectSRC)
+write_csv(check_answer_df, 'data/checkAnswer.csv')
+check_answer_df
+
 
 # finding trial attempts by trial and subject
 trialAttemptsByTrial <- check_answer_df %>% 
